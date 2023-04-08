06f2bb421aa13f05e08ae1dd997a0d7cc5bb3f4a
Rose Jang <jangmi749@gmail.com>
2023-04-08 17:27:25 +0900

    :memo: docs : 필요 데이터 추가

---
+++ src/assets/iconList.tsx
@@ -0,0 +1,23 @@
+interface Icon {
+  type: string;
+  name: string;
+  link: string;
+}
+
+export const iconList = [
+  {
+    type: "folder",
+    name: "Github",
+    link: "https://github.com/RoseJang2000",
+  },
+  {
+    type: "folder",
+    name: "Portfolio",
+    link: "https://rosejang2000-portfolio.vercel.app/",
+  },
+  {
+    type: "folder",
+    name: "Blog",
+    link: "https://rosejang2000.github.io/",
+  },
+];
