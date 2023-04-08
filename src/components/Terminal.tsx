7f2f949e20c4f12f0e03b4a8c275c2630f27f998
Rose Jang <jangmi749@gmail.com>
2023-04-08 19:22:25 +0900

    :sparkles: feat : draggable handle, bounds 속성 추가

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import Draggable from "react-draggable";
-import { useRef, useState } from "react";
+import { useRef } from "react";
 
 const Terminal = () => {
   const dragRef = useRef<HTMLDivElement>(null);
@@ -8,7 +8,7 @@ const Terminal = () => {
   return (
     <Draggable nodeRef={dragRef} handle=".bar" bounds="body">
       <TerminalContainer ref={dragRef}>
-        <div className="bar"> .bar</div>
+        <div className="bar"></div>
       </TerminalContainer>
     </Draggable>
   );
@@ -25,13 +25,15 @@ const TerminalContainer = styled.div`
   display: flex;
   flex-direction: column;
   overflow: hidden;
-  color: #fff;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
+  border: 1px solid #888;
   cursor: text;
 
   .bar {
     width: 100%;
     height: 2rem;
-    background-color: lightgray;
+    background-color: #777;
+    border-bottom: 1px solid #888;
     color: #000;
     cursor: default;
   }
