c5fe86319a331d19aa5e564b0aca33fa724ec8e5
Rose Jang <jangmi749@gmail.com>
2023-04-08 20:25:26 +0900

    :sparkles: feat : resizable box 적용

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -1,42 +1,63 @@
 import styled from "styled-components";
 import Draggable from "react-draggable";
-import { useRef } from "react";
+import { ResizableBox } from "react-resizable";
+import { useRef, useState } from "react";
+import { IoFilterOutline } from "react-icons/io5";
 
 const Terminal = () => {
-  const dragRef = useRef<HTMLDivElement>(null);
-
   return (
-    <Draggable nodeRef={dragRef} handle=".bar" bounds="body">
-      <TerminalContainer ref={dragRef}>
-        <div className="bar"></div>
-      </TerminalContainer>
-    </Draggable>
+    <TerminalWrapper>
+      <Draggable bounds="body" handle=".bar">
+        <ResizableBox
+          width={500}
+          height={400}
+          handle={
+            <ResizeHandle>
+              <IoFilterOutline size={25} />
+            </ResizeHandle>
+          }
+        >
+          <div className="bar"></div>
+        </ResizableBox>
+      </Draggable>
+    </TerminalWrapper>
   );
 };
 
-const TerminalContainer = styled.div`
-  width: 30rem;
-  height: 20rem;
-  background-color: black;
-  border-radius: 0.5rem;
+const TerminalWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
   position: absolute;
-  z-index: 2;
-  margin: auto;
-  display: flex;
-  flex-direction: column;
-  overflow: hidden;
-  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
-  border: 1px solid #888;
-  cursor: text;
+
+  .react-resizable {
+    position: relative;
+    width: 500px;
+    height: 400px;
+    background-color: #111;
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
+    border: 1px solid #888;
+    border-radius: 0.5rem;
+    overflow: hidden;
+    z-index: 10;
+  }
 
   .bar {
     width: 100%;
     height: 2rem;
-    background-color: #777;
     border-bottom: 1px solid #888;
     color: #000;
+    background-color: #777;
     cursor: default;
   }
+`;
+
+const ResizeHandle = styled.div`
+  position: absolute;
+  bottom: -0.3rem;
+  right: -0.3rem;
+  cursor: nw-resize;
+  color: #888;
+  transform: rotate(-46deg);
 `;
 
 export default Terminal;
