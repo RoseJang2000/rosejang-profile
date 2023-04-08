b3550a7eb0e082ad742e68e9e5426e7bd89292a4
Rose Jang <jangmi749@gmail.com>
2023-04-08 17:30:18 +0900

    :lipstick: design : 컨테이너 및 배경 디자인 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
+import Background from "@components/Background";
+import styled from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
 
 const App = () => {
   return (
-    <>
+    <Container>
       <GlobalStyle />
-    </>
+      <Background />
+      <section className="content">Hello! RoseJang2000!</section>
+    </Container>
   );
 };
+
+const Container = styled.main`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 3rem;
+
+  .content {
+    width: 20rem;
+    height: 7rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgba(255, 255, 255, 0.8);
+    border-radius: 0.5rem;
+    box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.25);
+  }
+`;
 
 export default App;
